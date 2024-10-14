be3c8b8757a3e2445593c48867a4fd581d5d4403
graham1776 <graham1776@gmail.com>
2024-10-14 15:01:51 -0700

    new chat gpt attempt to get crawler to work

--- rss.js
+++ rss.js
@@ -1,4 +1,4 @@
-var feedsLibrary = {
+const feedsLibrary = {
   prea: "http://fetchrss.com/rss/5ad0f27c8a93f8e6778b4567842180607.xml",
   nareit: "http://fetchrss.com/rss/5ad0f27c8a93f8e6778b4567140357561.xml",
   bre: "http://fetchrss.com/rss/5ad0f27c8a93f8e6778b4567918028761.xml",
@@ -14,28 +14,40 @@ var feedsLibrary = {
   globest: "http://feeds.feedblitz.com/globest/national",
   rentv: "http://fetchrss.com/rss/5ad0f27c8a93f8e6778b4567239708145.xml",
   rcm: "http://fetchrss.com/rss/5ad0f27c8a93f8e6778b4567365828158.xml",
+};
+
+async function parseRSS(url, callback) {
+  const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
+
+  try {
+    const response = await fetch(proxyUrl);
+    if (!response.ok) throw new Error("Network response was not ok");
+
+    const data = await response.json();
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(data.contents, "application/xml");
+
+    callback(xml);
+  } catch (error) {
+    console.error("Failed to fetch RSS feed:", error);
+  }
 }
 
-function parseRSS(url, callback) {
-  var proxyUrl = "https://api.allorigins.win/get?url=" + encodeURIComponent(url);
-  $.getJSON(proxyUrl, function (data) {
-    var xml = $.parseXML(data.contents);
-    callback($(xml));
-  });
-}
+Object.entries(feedsLibrary).forEach(([feedName, feedUrl]) => {
+  parseRSS(feedUrl, (xml) => {
+    const feedItems = xml.querySelectorAll("item");
 
-$.each(feedsLibrary, function (feedName, feedUrl) {
-  parseRSS(feedUrl, function (xml) {
-    var feedItems = xml.find("item");
+    feedItems.forEach((item) => {
+      const title = item.querySelector("title").textContent;
+      const link = item.querySelector("link").textContent;
 
-    $.each(feedItems, function () {
-      var item = $(this);
-      var title = item.find("title").text();
-      var link = item.find("link").text();
-
-      var ulElement = $("#" + feedName);
-      ulElement.append('<li class="feed-item"><a class="feed-link" target="_blank" href="' + link + '">' + title + "</a></li>");
+      const ulElement = document.getElementById(feedName);
+      if (ulElement) {
+        ulElement.insertAdjacentHTML(
+          "beforeend",
+          `<li class="feed-item"><a class="feed-link" target="_blank" href="${link}">${title}</a></li>`
+        );
+      }
     });
   });
 });
-
